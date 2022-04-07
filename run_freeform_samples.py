b8c043149b9addc0bde4a9e8e49d268bfc9e0cee
Conor Nugent <nugentc@gmail.com>
2022-04-07 13:45:33 +0100

    add a visualisation treatment for vertical text

--- run_freeform_samples.py
+++ run_freeform_samples.py
@@ -24,6 +24,61 @@ def hconcat(im1, im2):
     return dst
 
 
+
+
+def draw_rotated_text(image, angle, xy, text, fill, *args, **kwargs):
+    """ Draw text at an angle into an image, takes the same arguments
+        as Image.text() except for:
+
+    :param image: Image to write text into
+    :param angle: Angle to write text at
+
+    code originally found in answewr supplied by Stephen Rauch here- https://stackoverflow.com/questions/45179820/draw-text-on-an-angle-rotated-in-python
+    """
+    # get the size of our image
+    width, height = image.size
+    max_dim = max(width, height)
+
+    # build a transparency mask large enough to hold the text
+    mask_size = (max_dim * 2, max_dim * 2)
+    mask = Image.new('L', mask_size, 0)
+
+    # add text to mask
+    draw = ImageDraw.Draw(mask)
+    draw.text((max_dim, max_dim), text, 255, *args, **kwargs)
+
+    if angle % 90 == 0:
+        # rotate by multiple of 90 deg is easier
+        rotated_mask = mask.rotate(angle)
+    else:
+        # rotate an an enlarged mask to minimize jaggies
+        bigger_mask = mask.resize((max_dim*8, max_dim*8),
+                                  resample=Image.BICUBIC)
+        rotated_mask = bigger_mask.rotate(angle).resize(
+            mask_size, resample=Image.LANCZOS)
+
+    # crop the mask to match image
+    mask_xy = (max_dim - xy[0], max_dim - xy[1])
+    b_box = mask_xy + (mask_xy[0] + width, mask_xy[1] + height)
+    mask = rotated_mask.crop(b_box)
+
+    # paste the appropriate color, with the text transparency mask
+    color_image = Image.new('RGBA', image.size, fill)
+    image.paste(color_image, mask)
+
+
+def find_font_size(text_width, text, font_file):
+    font_size = text_width
+    font = ImageFont.truetype(font_file, font_size)
+    size = font.getsize_multiline(text)[0]
+    while size > text_width:
+        diff = 20 if size - text_width > 100 else 1
+        font_size = font_size - diff
+        font = ImageFont.truetype(font_file, font_size)
+        size = font.getsize_multiline(text)[0]
+    return font 
+
+
 def visualise_ocr(data, original_image, language):
     """
     Visualise DEEPREAD Free Form response in side-by-side image comparison.
@@ -40,28 +95,29 @@ def visualise_ocr(data, original_image, language):
     editable_image = ImageDraw.Draw(output_image)
 
     font_file = os.path.join("fonts", "COURIER.ttf")
+
     if language == 'ja':
         font_file = os.path.join("fonts", "Arial Unicode MS.TTF")
 
     for area in data['pages'][0]['areas']:
         for para in area['paragraphs']:
             for line in para['lines']:
+                vertical_text = line['coordinates']['w'] < line['coordinates']['h']
+
                 for word in line['words']:
                     coords = word['coordinates']
+                    text = word['text']
                     bbox = [coords['x'], 
                             coords['y']]
-
-                    bounding_box_size = math.floor((coords['w']))
-                    font_size = bounding_box_size
-                    font = ImageFont.truetype(font_file, font_size)
-                    size = font.getsize_multiline(word['text'])[0]
-                    while size > bounding_box_size:
-                        diff = 20 if size - bounding_box_size > 100 else 1
-                        font_size = font_size - diff
-                        font = ImageFont.truetype(font_file, font_size)
-                        size = font.getsize_multiline(word['text'])[0]
-
-                    editable_image.text((bbox[0], bbox[1]), word['text'], (0, 0, 0), font=font)
+                    
+                    if vertical_text:
+                        bounding_box_size = math.floor((coords['h']))
+                        font = find_font_size(bounding_box_size, text, font_file)
+                        draw_rotated_text(output_image, 90, (bbox[0], bbox[1]), text, (0, 0, 0), font=font)
+                    else:
+                        bounding_box_size = math.floor((coords['w']))
+                        font = find_font_size(bounding_box_size, text, font_file)
+                        editable_image.text((bbox[0], bbox[1]), text, (0, 0, 0), font=font)
     return hconcat(output_image, original_image)
 
 
